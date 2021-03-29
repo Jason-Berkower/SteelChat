219e672ecc124da32a7c61ecc858c429399bf24d
Jason Berkower <jasonberkower@Jasons-iMac.home>
2021-03-29 16:53:29 -0400

    Set groundwork for second message API.

--- src/services/Index.js
+++ src/services/Index.js
@@ -1,5 +1,7 @@
 export const baseURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/users`;
 
+export const baseURL2 = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE2}/messages`;
+
 export const config = {
   headers: {
     Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
