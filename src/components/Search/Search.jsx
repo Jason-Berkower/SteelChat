5043529c31432dfb72f3c82c3f35c365c7d5cfff
Jason Berkower <jasonberkower@Jasons-iMac.home>
2021-04-01 16:22:55 -0400

    Search components implemented. Just need to address functionality errors.

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,44 +1,30 @@
-import { useState, useEffect } from 'react';
-import { baseURL2, config } from '../../services/Index';
-import { useHistory, useParams } from 'react-router-dom';
-import axios from 'axios';
 import './Search.css';
 
 function Search(props) {
-  let [search, setSearch] = useState([]);
-  const history = useHistory();
-  const params = useParams();
-
-  useEffect(() => {
-    if (params.id && props.messages.length > 0) {
-      const searchResult = props.messages.find((message) => {
-        return message.id === params.id;
-      });
-      setSearch(searchResult.fields);
-    }
-  }, [params.id, props.messages])
+  let { searchTerm, setSearchTerm, setFilteredMessages, messages } = props;
 
   function handleChange(event) {
-    let { name, value } = event.target;
-    setSearch((prevState) => ({ ...prevState, [name]: value }));
-  }
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-    if (params.id) {
-      let results = `${baseURL2}/${params.id}`;
-      await axios.put(results, { fields: search }, config)
-    }
-    props.setToggle((prevState) => !prevState);
-    history.pushState('/');
-  }
+    setSearchTerm(event.target.value);
+    setFilteredMessages(
+      messages.filter((message) => {
+        message.fields.message
+          .toLowerCase()
+          .replace(/ /g, "")
+          .includes(searchTerm.replace(/ /g, ""))
+      })
+    )
+  };
 
   return (
     <div>
-      <form className="search" onSubmit={handleSubmit}>
-        <input placeholder="Search Posts" onChange={handleChange} />
-        <input id='submit' type='submit' />
-      </form>
+      <label htmlFor='search'>Search</label>
+      <input
+        type='text'
+        name='search'
+        id='search'
+        value={searchTerm}
+        onChange={handleChange}
+      />
     </div>
   )
 }
